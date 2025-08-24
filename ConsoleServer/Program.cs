d260c8295b714af2a19434fba59f89070d128036
Taras <arikcarikc4@gmail.com>
2025-08-24 12:19:15 +0300

    # Додавання коментарів  у Program .cs  та Form1

--- ConsoleServer/Program.cs
+++ ConsoleServer/Program.cs
@@ -20,6 +20,7 @@ class Program
         Console.ReadLine();
     }
 
+    //  Метод виконуе роль UDP сервера
     private static async void WaitClientQuery()
     {
         UdpClient server = new UdpClient(49152);
