b193ba38b2f6dfa135c1d15f5c89d9f6aab643af
Тaras <arikcarikc4@gmail.com>
2025-08-24 11:19:42 +0300

    #Метод виконуе роль UDP-сервера
    чекає запитів від клієнтів, обробляє їх і надсилає відповіді

--- ConsoleServer/Program.cs
+++ ConsoleServer/Program.cs
@@ -20,5 +20,41 @@ class Program
         Console.ReadLine();
     }
 
-  
+    private static async void WaitClientQuery()
+    {
+        UdpClient server = new UdpClient(49152);
+
+        while (true)
+        {
+            try
+            {
+                UdpReceiveResult result = await server.ReceiveAsync();
+                IPEndPoint clientEndPoint = result.RemoteEndPoint;
+                byte[] data = result.Buffer;
+
+                if (data.Length > 0)
+                {
+                    MemoryStream stream = new MemoryStream(data);
+                    XmlSerializer serializer = new XmlSerializer(typeof(Message));
+                    Message message = (Message)serializer.Deserialize(stream);
+                    stream.Close();
+
+                    Console.WriteLine($"Підключено: IP: {clientEndPoint.Address}, Port: {clientEndPoint.Port}, User: {message.user}, Time: {DateTime.Now}");
+
+                    // Обробка запиту
+                    string[] currencies = message.mes.Split(' ');
+                    string response = CalculateExchangeRate(currencies[0], currencies[1]);
+                    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+
+                    // Відправка відповіді
+                    await server.SendAsync(responseBytes, responseBytes.Length, clientEndPoint);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Помилка: {ex.Message}");
+            }
+        }
+    }
+
 }
